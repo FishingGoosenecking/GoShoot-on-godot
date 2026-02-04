e6f665f265c0ba20b6952ef3832ab83f85442cf8
FishingGoosenecking <palmpalm7647@gmail.com>
2026-02-04 10:35:08 +0700

    prep for git pages

---
+++ docs/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="dark">
+<head>
+    <?php include("include.php"); ?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index</title>
+    <?php include("navbar.php"); ?>
+</head>
+<body>
+    
+</body>
+</html>
